78003f14558f2cabe09ce8efe0273ebf28345e8b
Kpamiose Uchechukwu Obed <engr.austinuche@gmail.com>
2024-03-24 07:23:27 +0100

    made the write-ups to show properly on both small and large screens

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,13 @@ import { MdOutlineCurrencyExchange } from 'react-icons/md'
 const Footer = () => {
   return (
       <section className="w-full lg:w-full m-auto bg-darkBg">
-          <div className="w-[90%] lg:w-[80%] m-auto flex lg:flex-row justify-around items-center py-1 text-textColor bg-darkBg">
-              <div>©Copyright 2024 247exchange. All rights reserved.</div>
-              <div className='flex text-3xl font-extrabold'>247<MdOutlineCurrencyExchange />xchange</div>
+          <div className="w-full lg:w-[80%] m-auto flex flex-col-reverse lg:flex-row justify-center lg:justify-between gap-2 items-center py-1 text-textColor bg-darkBg">
+              <div className="w-full text-center leading-4">©Copyright 2024 247exchange. All rights reserved.</div>
+              <div className="w-full text-center justify-center flex text-xl lg:text-3xl font-extrabold">
+                  247
+                  <MdOutlineCurrencyExchange />
+                  xchange
+              </div>
           </div>
       </section>
   )
