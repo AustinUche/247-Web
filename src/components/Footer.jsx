94ad3410180791f7487462d1c8a2f7b605a23af6
Kpamiose Uchechukwu Obed <engr.austinuche@gmail.com>
2024-03-24 19:07:50 +0100

    made the logo name and icon to align center

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <section className="w-full lg:w-full m-auto bg-darkBg">
           <div className="w-full lg:w-[80%] m-auto flex flex-col-reverse lg:flex-row justify-center lg:justify-between gap-2 items-center py-1 text-textColor bg-darkBg">
               <div className="w-full text-center leading-4">©Copyright 2024 247exchange. All rights reserved.</div>
-              <div className="w-full text-center justify-center flex text-xl lg:text-3xl font-extrabold">
+              <div className="w-full text-center justify-center flex text-xl lg:text-3xl font-extrabold items-center">
                   247
                   <MdOutlineCurrencyExchange />
                   xchange
